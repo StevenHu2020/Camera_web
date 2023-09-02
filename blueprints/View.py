605aaa1631004a6f597bcd2e49d9d32ec6210934
charlieJ107 <charlie_j107@outlook.com>
2023-09-02 22:52:26 +0100

    Fix title in the result table

--- blueprints/View.py
+++ blueprints/View.py
@@ -75,18 +75,17 @@ def result():
         Camera_Date = [i for i in Camera_Date if i.FrameAdvanceRate >= 7]
 
     # Reorganization of data
-    titles = [" ", "Name", "band","Type", "ImageSensor", "EffectivePixels", "FrameAdvanceRate", "ShutterSpeed", "Dimensions", "Weight", "Price"]
-    images = [camera.Herf for camera in Camera_Date]
-    names = [camera.Name for camera in Camera_Date]
-    brand = [camera.Brand for camera in Camera_Date]
-    Type = [camera.Type for camera in Camera_Date]
-    ImageSensor = [camera.ImageSensor for camera in Camera_Date]
-    EffectivePixels = [camera.EffectivePixels for camera in Camera_Date]
-    FrameAdvanceRate = [camera.FrameAdvanceRate for camera in Camera_Date]
-    ShutterSpeed = [camera.ShutterSpeed for camera in Camera_Date]
-    Dimensions = [camera.Dimensions for camera in Camera_Date]
-    Weight = [camera.Weight for camera in Camera_Date]
-    Price = [camera.Price for camera in Camera_Date]
+    images = [" "] +[camera.Herf for camera in Camera_Date]
+    names = ["Name"] + [camera.Name for camera in Camera_Date]
+    brand = ["Brand"] + [camera.Brand for camera in Camera_Date]
+    Type = ["Type"] + [camera.Type for camera in Camera_Date]
+    ImageSensor = ["Image Sensor"] + [camera.ImageSensor for camera in Camera_Date]
+    EffectivePixels = ["Effective Pixels"] + [camera.EffectivePixels for camera in Camera_Date]
+    FrameAdvanceRate = ["Frame Advance Rate"] + [camera.FrameAdvanceRate for camera in Camera_Date]
+    ShutterSpeed = ["Shutter Speed"] + [camera.ShutterSpeed for camera in Camera_Date]
+    Dimensions = ["Dimensions"] + [camera.Dimensions for camera in Camera_Date]
+    Weight = ["Weight"] + [camera.Weight for camera in Camera_Date]
+    Price = ["Price"] + [camera.Price for camera in Camera_Date]
 
     # translate it to a two-dimensional array
     # title need another group
